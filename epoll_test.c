6578a8149e81cb1037feb7b6fe2a696bb6ae54bc
Spongecaptain <wjjiang19@163.com>
2021-07-22 11:07:08 +0800

    update epoll

--- epoll_test.c
+++ epoll_test.c
@@ -24,19 +24,16 @@ int main(int argc, char *argv[])
       // 初始化服务端用于监听的 socket
       int listensock = initserver(PORT);
       printf("listensock=%d\n", listensock);
-
       if (listensock < 0)
       {
             printf("initserver() failed.\n");
             return -1;
       }
+      int epollfd;                       // epoll_create 函数返回的文件描述符
+      char buffer[1024];                 // 读写操作共用的 char[] 数组
+      memset(buffer, 0, sizeof(buffer)); // 将 buffer 初始化
 
-      int epollfd;
-
-      char buffer[1024];
-      memset(buffer, 0, sizeof(buffer));
-
-      // 创建一个描述符
+      // 创建一个 epoll 文件描述符，区别：select 以及 poll 都不会返回一个文件描述符
       epollfd = epoll_create(1);
 
       // 添加监听描述符事件
@@ -48,31 +45,27 @@ int main(int argc, char *argv[])
       while (1)
       {
             struct epoll_event events[MAXEVENTS]; // 存放有事件发生的结构数组
-
             // 等待监视的 socket 有事件发生
+            // epoll_wait 函数返回时会对 epoll_event 结构体数组产生副作用
             int infds = epoll_wait(epollfd, events, MAXEVENTS, -1);
-            // printf("epoll_wait infds=%d\n",infds);
-
-            // 返回失败
+            // <0  表示出现致命错误
             if (infds < 0)
             {
                   printf("epoll_wait() failed.\n");
                   perror("epoll_wait()");
                   break;
             }
-
-            // 超时
+            // =0 表示超时
             if (infds == 0)
             {
                   printf("epoll_wait() timeout.\n");
                   continue;
             }
-
             // 遍历有事件发生的结构数组
-            int ii;
-            for (ii = 0; ii < infds; ii++)
+            int j;
+            for (j = 0; j < infds; j++)
             {
-                  if ((events[ii].data.fd == listensock) && (events[ii].events & EPOLLIN))
+                  if ((events[j].data.fd == listensock) && (events[j].events & EPOLLIN))
                   {
                         // 如果发生事件的是 listensock，表示有新的客户端连上来
                         struct sockaddr_in client;
@@ -89,38 +82,34 @@ int main(int argc, char *argv[])
                         ev.data.fd = clientsock;
                         ev.events = EPOLLIN;
                         epoll_ctl(epollfd, EPOLL_CTL_ADD, clientsock, &ev);
-
                         printf("client(socket=%d) connected ok.\n", clientsock);
-
                         continue;
                   }
-                  else if (events[ii].events & EPOLLIN)
+                  else if (events[j].events & EPOLLIN)
                   {
                         // 客户端有数据过来或客户端的 socket 连接被断开
                         char buffer[1024];
                         memset(buffer, 0, sizeof(buffer));
 
-                        // 读取客户端的数据
-                        ssize_t isize = read(events[ii].data.fd, buffer, sizeof(buffer));
+                        // 读取客户端的数据，一次最多读取 1024 字节
+                        ssize_t isize = read(events[j].data.fd, buffer, sizeof(buffer));
 
                         // 发生了错误或 socket 被对方关闭
                         if (isize <= 0)
                         {
-                              printf("client(eventfd=%d) disconnected.\n", events[ii].data.fd);
-
+                              printf("client(eventfd=%d) disconnected.\n", events[j].data.fd);
                               // 把已断开的客户端从 epoll 中删除
                               memset(&ev, 0, sizeof(struct epoll_event));
                               ev.events = EPOLLIN;
-                              ev.data.fd = events[ii].data.fd;
-                              epoll_ctl(epollfd, EPOLL_CTL_DEL, events[ii].data.fd, &ev);
-                              close(events[ii].data.fd);
+                              ev.data.fd = events[j].data.fd;
+                              epoll_ctl(epollfd, EPOLL_CTL_DEL, events[j].data.fd, &ev);
+                              close(events[j].data.fd);
                               continue;
                         }
-
-                        printf("recv(eventfd=%d,size=%d):%s\n", events[ii].data.fd, isize, buffer);
-
+                        printf("recv(eventfd=%d,size=%d):%s\n", events[j].data.fd, isize, buffer);
                         // 把收到的报文发回给客户端
-                        write(events[ii].data.fd, buffer, strlen(buffer));
+                        // 通过事件就能够找到对应的文件描述符，这是 epoll 与 select、poll 的区别之一
+                        write(events[j].data.fd, buffer, strlen(buffer));
                   }
             }
       }
@@ -140,7 +129,7 @@ int initserver(int port)
             return -1;
       }
 
-      // Linux如下
+      // Linux 如下
       int opt = 1;
       unsigned int len = sizeof(opt);
       setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &opt, len);
